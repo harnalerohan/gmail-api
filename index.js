7ed17b92151ecbf299a6a1f8e1aebc8377249c3c
RON\harna <harnalerohan@gmail.com>
2021-09-21 19:54:44 +0530

    added a root route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.json());
 app.use('/app', Router);
 
+app.get('/', (req, res) => {
+  res.send("Welcome to gmail api app")
+})
+
 app.listen(8000, () => {
   console.log("server listening on port 8000")
 });
